c359191af83e768692614b04ac53ba365bc15a71
jgblanc <jgblanc@uchicago.edu>
2024-07-15 16:39:33 -0500

    t

--- code/calculate_FGr/compute_PC_FGR_model.R
+++ code/calculate_FGr/compute_PC_FGR_model.R
@@ -16,6 +16,9 @@ PC_file = args[2]
 outfile = args[3]
 test_type = args[4]
 
+# Get test type
+test_type <- strsplit(test_type, "-")[[1]][2]
+print(test_type)
 
 # Get list of correct chromosomes
 if (test_type == "all") {
@@ -29,7 +32,7 @@ print(chrs)
 
 # Add FGr together for all the blocks on chrtype chromosomes
 tmp <- fread(paste0(Tm_prefix, "_", chrs[1], ".txt"))
-df <- tmp[,1:4]
+df <- tmp[,1:3]
 df$FGr <- rowSums(tmp[,4:ncol(tmp)])
 for (i in 2:length(chrs)) {
 
@@ -41,30 +44,28 @@ df$FGr <- scale(df$FGr)
 
 # Join with PCs
 PCs <- fread(PC_file)
+PC_IDs <- PCs[,1:2]
+PC_nums <- PCs[,3:ncol(PCs)]
 
 # Scale the PCs to have variance 1
-PCs[,3:ncol(PCs)] <- apply(PCs[,3:ncol(PCs)], 2, as.numeric)
-PCs[,3:ncol(PCs)] <- scale(PCs[,3:ncol(PCs)])
-print(paste0("The var of the PC cols are ", apply(PCs[,3:ncol(PCs)], 2, var)))
+PC_nums <- scale(PC_nums)
+#print(paste0("The var of the PC cols are ", apply(PC_nums, 2, var)))
 
 # Combine Dataframes
-dfCombine <- inner_join(df, PCs)
+dfPCs <- cbind(PC_IDs, PC_nums)
+dfCombine <- inner_join(df, dfPCs)
 print(colnames(dfCombine))
-print(head(dfCombine[,4]))
-print(head(dfCombine[,43]))
+
 
 # Construct data frame to collate results
 totalPCs <-  (ncol(PCs) - 2)
+print(totalPCs)
 dfOut <- matrix(NA, nrow = totalPCs, ncol = 8)
 dfOut[,1] <- seq(1,totalPCs)
 
 # Compute multiple R^2 and rho(PC, FGr)
 for (i in 1:nrow(dfOut)) {
 
-  # Compute variance explained
-  mod <- lm(dfCombine$FGr ~ . ,data=dfCombine[,4:(i+2)])
-  r2_model <- cor(dfCombine$FGr, fitted(mod))^2
-  print(r2_model)
 
   # Compute Bi
   name <- paste0("PC", i)
@@ -76,26 +77,23 @@ for (i in 1:nrow(dfOut)) {
   # Compute correlation
   c1 <- dfCombine[[name]]
   ct <- cor.test(as.numeric(dfCombine$FGr),as.numeric(c1))
-  rho <- ct$estimate
   lc <- ct$conf.int[1]
   uc <- ct$conf.int[2]
 
 
   # Collect output
-  dfOut[i,2] <- r2_model
-  dfOut[i,3] <- B
-  dfOut[i,4] <- B^2
-  dfOut[i,5] <- rho
-  dfOut[i,6] <- lc
-  dfOut[i,7] <- uc
-  dfOut[i,8] <- sum(dfOut[1:i, 4])
+  dfOut[i,2] <- B
+  dfOut[i,3] <- B^2
+  dfOut[i,4] <- lc
+  dfOut[i,5] <- uc
+  dfOut[i,6] <- sum(dfOut[1:i, 3])
 
 }
 
 
 # Save output
 dfOut <- as.data.frame(dfOut)
-colnames(dfOut) <- c("PC", "r2_model", "B", "B2", "rho", "lc", "uc", "r2_sum")
+colnames(dfOut) <- c("PC", "B", "B2", "lc", "uc", "r2")
 fwrite(dfOut, outfile, row.names = F, col.names = T, quote = F, sep = "\t")
 
 
