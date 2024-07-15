a7d53a4a11b0ec988dbf6902c5e6ce3b0c1e2b49
Jennifer Blanc <jgblanc@uchicago.edu>
2024-07-15 16:12:59 -0500

    t

--- code/calculate_FGr/compute_PC_FGR_model.R
+++ code/calculate_FGr/compute_PC_FGR_model.R
@@ -28,60 +28,74 @@ if (test_type == "all") {
 print(chrs)
 
 # Add FGr together for all the blocks on chrtype chromosomes
-df <- fread(paste0(Tm_prefix, "_", chrs[1], ".txt"))
+tmp <- fread(paste0(Tm_prefix, "_", chrs[1], ".txt"))
+df <- tmp[,1:4]
+df$FGr <- rowSums(tmp[,4:ncol(tmp)])
 for (i in 2:length(chrs)) {
 
-  new <- fread(paste0(Tm_prefix, "_", chrs[i], ".txt"))
-  df$FGr <- df$FGr + new$FGr
+  tmp <- fread(paste0(Tm_prefix, "_", chrs[i], ".txt"))
+  df$FGr <- df$FGr + rowSums(tmp[,4:ncol(tmp)])
 
 }
-
 df$FGr <- scale(df$FGr)
 
 # Join with PCs
 PCs <- fread(PC_file)
-dfCombine <- inner_join(PCs, df)
-print(nrow(dfCombine))
-print(head(dfCombine[,3]))
-print(head(dfCombine[,42]))
+
+# Scale the PCs to have variance 1
+PCs[,3:ncol(PCs)] <- apply(PCs[,3:ncol(PCs)], 2, as.numeric)
+PCs[,3:ncol(PCs)] <- scale(PCs[,3:ncol(PCs)])
+print(paste0("The var of the PC cols are ", apply(PCs[,3:ncol(PCs)], 2, var)))
+
+# Combine Dataframes
+dfCombine <- inner_join(df, PCs)
+print(colnames(dfCombine))
+print(head(dfCombine[,4]))
+print(head(dfCombine[,43]))
 
 # Construct data frame to collate results
-dfOut <- matrix(NA, nrow = 40, ncol = 5)
-dfOut[,1] <- seq(1,40)
+totalPCs <-  (ncol(PCs) - 2)
+dfOut <- matrix(NA, nrow = totalPCs, ncol = 8)
+dfOut[,1] <- seq(1,totalPCs)
 
 # Compute multiple R^2 and rho(PC, FGr)
 for (i in 1:nrow(dfOut)) {
 
-
   # Compute variance explained
-  mod <- lm(dfCombine$FGr ~ . ,data=dfCombine[,3:(i+2)])
-  r2 <- cor(dfCombine$FGr, fitted(mod))^2
-  print(r2)
+  mod <- lm(dfCombine$FGr ~ . ,data=dfCombine[,4:(i+2)])
+  r2_model <- cor(dfCombine$FGr, fitted(mod))^2
+  print(r2_model)
 
-  # Compute correlation
-  #name <- paste0("PC_", i)
+  # Compute Bi
   name <- paste0("PC", i)
   print(name)
   c1 <- dfCombine[[name]]
-  print(length(c1))
-  print(length(dfCombine$FGr))
+  B <- cov(dfCombine$FGr, c1)
+  B2 <- B^2
+
+  # Compute correlation
+  c1 <- dfCombine[[name]]
   ct <- cor.test(as.numeric(dfCombine$FGr),as.numeric(c1))
   rho <- ct$estimate
   lc <- ct$conf.int[1]
   uc <- ct$conf.int[2]
 
+
   # Collect output
-  dfOut[i,2] <- r2
-  dfOut[i,3] <- rho
-  dfOut[i,4] <- lc
-  dfOut[i,5] <- uc
+  dfOut[i,2] <- r2_model
+  dfOut[i,3] <- B
+  dfOut[i,4] <- B^2
+  dfOut[i,5] <- rho
+  dfOut[i,6] <- lc
+  dfOut[i,7] <- uc
+  dfOut[i,8] <- sum(dfOut[1:i, 4])
 
 }
 
 
 # Save output
 dfOut <- as.data.frame(dfOut)
-colnames(dfOut) <- c("PC", "r2", "rho", "lc", "up")
+colnames(dfOut) <- c("PC", "r2_model", "B", "B2", "rho", "lc", "uc", "r2_sum")
 fwrite(dfOut, outfile, row.names = F, col.names = T, quote = F, sep = "\t")
 
 
