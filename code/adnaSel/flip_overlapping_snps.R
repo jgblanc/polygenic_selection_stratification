de9b3ca9d689710baed218d7607cb1101929e213
jgblanc <jgblanc@uchicago.edu>
2024-07-25 19:31:02 -0500

    t

--- code/adnaSel/flip_overlapping_snps.R
+++ code/adnaSel/flip_overlapping_snps.R
@@ -13,25 +13,26 @@ suppressWarnings(suppressMessages({
 ukbb_file = args[1]
 data_file = args[2]
 r_outpath = args[3]
-snp_outfile = args[4]
+snp_outpath = args[4]
 chr_num = args[5]
 print(chr_num)
 
 
 # Read in UKBB freq file
 ukbb <- fread(ukbb_file)
-ukbb <- ukbb %>% separate(ID, c("tmp", "POS"), remove = FALSE) %>% filter(ukbb_wbs$ALT_FREQS > 0.01 & ukbb_wbs$ALT_FREQS < 0.99) %>% select("#CHROM",	"ID","POS","REF",	"ALT")
+ukbb <- ukbb %>% separate(ID, c("tmp", "POS"), remove = FALSE) %>% filter(ukbb$ALT_FREQS > 0.01 & ukbb$ALT_FREQS < 0.99) %>% select("#CHROM",	"ID","POS","REF",	"ALT")
 colnames(ukbb)[1] <- "CHROM"
 ukbb <- ukbb %>% separate(ID, c("tmp", "POS"), remove = FALSE)
 ukbb$POS <- as.numeric(ukbb$POS)
 
 # Read in contrasts
-sds <- fread(sds_file)
-sds <- sds %>% filter(CHROM == as.integer(chr_num))
+data <- fread(data_file)
+data <- data %>% filter(CHROM == as.integer(chr_num))
+print(nrow(data))
 
 # Join files by chromosome and position
-df <- inner_join(ukbb, sds)
-head(df)
+df <- inner_join(ukbb, data)
+head(nrow(df))
 
 # Change allele frequency to alternate allele freq by taking 1 -
 df_flipped <- df %>% mutate(EUROPEAN_NEOLITHIC = 1 - EUROPEAN_NEOLITHIC, BRONZE_AGE = 1 - BRONZE_AGE, HISTORICAL = 1 - HISTORICAL,
@@ -45,19 +46,19 @@ df_H <- df_flipped %>% mutate(expected = (0.15*EUROPEAN_NEOLITHIC) + (0.85*BRONZ
 # Select correct columns and save output
 
 df_EN <- df_EN %>% select("CHROM", "ID", "REF", "ALT", "ss")
-colnames(df_EN) <- c("CHROM", "ID", "REF", "ALT", "r")
-fwrite(df_EN, paste0(r_outpath,"EUROPEAN_NEOLITHIC_chr", chr_num, ".rvec"), row.names = F, col.names = T, quote = F, sep = "\t")
+colnames(df_EN) <- c("#CHROM", "ID", "REF", "ALT", "r")
+fwrite(df_EN, paste0(r_outpath,"EuropeanNeolithic_chr", chr_num, ".rvec"), row.names = F, col.names = T, quote = F, sep = "\t")
 
 df_BA <- df_BA %>% select("CHROM", "ID", "REF", "ALT", "ss")
-colnames(df_BA) <- c("CHROM", "ID", "REF", "ALT", "r")
-fwrite(df_BA, paste0(r_outpath,"BRONZE_AGE_chr", chr_num, ".rvec"), row.names = F, col.names = T, quote = F, sep = "\t")
+colnames(df_BA) <- c("#CHROM", "ID", "REF", "ALT", "r")
+fwrite(df_BA, paste0(r_outpath,"BronzeAge_chr", chr_num, ".rvec"), row.names = F, col.names = T, quote = F, sep = "\t")
 
 df_H <- df_H %>% select("CHROM", "ID", "REF", "ALT", "ss")
-colnames(df_H) <- c("CHROM", "ID", "REF", "ALT", "r")
-fwrite(df_H, paste0(r_outpath,"HISTORICAL_chr", chr_num, ".rvec"), row.names = F, col.names = T, quote = F, sep = "\t")
+colnames(df_H) <- c("#CHROM", "ID", "REF", "ALT", "r")
+fwrite(df_H, paste0(r_outpath,"Historical_chr", chr_num, ".rvec"), row.names = F, col.names = T, quote = F, sep = "\t")
 
 # Save set of overlapping SNPs
-dfSNP <- dfEN %>% select("ID")
-fwrite(dfSNP, snp_outfile, row.names = F, col.names = T, quote = F, sep = "\t")
-
-
+dfSNP <- df_flipped %>% select("ID")
+fwrite(dfSNP, paste0(snp_outpath,"Historical/overlappingSNPs_chr", chr_num, ".txt"), row.names = F, col.names = T, quote = F, sep = "\t")
+fwrite(dfSNP, paste0(snp_outpath,"BronzeAge/overlappingSNPs_chr", chr_num, ".txt"), row.names = F, col.names = T, quote = F, sep = "\t")
+fwrite(dfSNP, paste0(snp_outpath,"EuropeanNeolithic/overlappingSNPs_chr", chr_num, ".txt"), row.names = F, col.names = T, quote = F, sep = "\t")
