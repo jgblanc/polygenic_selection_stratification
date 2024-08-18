68b32a63e6534fe8dfc33c5c0e6ee42d58a772ef
jgblanc <jgblanc@uchicago.edu>
2024-08-18 18:37:53 -0500

    t

--- code/calculate_FGr/sample_snps.R
+++ code/calculate_FGr/sample_snps.R
@@ -11,19 +11,36 @@ suppressWarnings(suppressMessages({
 
 outAll = args[1]
 outSample = args[2]
-nsnp = as.numeric(strsplit(args[3], "L-")[[1]][2])
+nsnp = strsplit(args[3], "L-")[[1]][2]
 
-df <- fread(args[4], header=FALSE)
-print(df)
-for (i in 5:length(args)) {
-  tmp <- fread(args[i], header=FALSE)
-  df <- rbind(df, tmp)
+if (nsnp == "all") {
+   df <- fread(args[26], header=FALSE)
+   print(args[26])
+   for (i in 27:length(args)) {
+       tmp <- fread(args[i], header=FALSE)
+       df <- rbind(df, tmp)
+       }
+   fwrite(df, outSample ,row.names=F,quote=F,sep="\t", col.names = T)
+   fwrite(df, outAll ,row.names=F,quote=F,sep="\t", col.names = T)
+} else if (nsnp == "pruneall") {
+   df <- fread(args[4], header=FALSE)
+   for (i in 5:25) {
+       tmp <- fread(args[i], header=FALSE)
+       df <- rbind(df, tmp)
+       }
+   fwrite(df, outAll ,row.names=F,quote=F,sep="\t", col.names = T)
+   fwrite(df, outSample ,row.names=F,quote=F,sep="\t", col.names = T)
+} else {
+  nsnp = as.numeric(nsnp)
+  df <- fread(args[4], header=FALSE)
+  for (i in 5:25) {
+      tmp <- fread(args[i], header=FALSE)
+      df <- rbind(df, tmp)
+      }
+  fwrite(df, outAll ,row.names=F,quote=F,sep="\t", col.names = T)
+  df <- df %>% sample_n(nsnp)
+  fwrite(df, outSample ,row.names=F,quote=F,sep="\t", col.names = T)
 }
-fwrite(df, outAll ,row.names=F,quote=F,sep="\t", col.names = T)
-print(nrow(df))
-
-df <- df %>% sample_n(nsnp)
-fwrite(df, outSample ,row.names=F,quote=F,sep="\t", col.names = T)
 
 
 
