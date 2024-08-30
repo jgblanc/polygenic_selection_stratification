665bf3cd292f5383c1d35c7f4a68ab479a1cc664
jgblanc <jgblanc@uchicago.edu>
2024-08-30 12:35:06 -0500

    t

--- code/calculate_FGr/separate_even_odd.R
+++ code/calculate_FGr/separate_even_odd.R
@@ -6,26 +6,28 @@ if(length(args)<3){stop("Rscript get_IDs.R")}
 
 suppressWarnings(suppressMessages({
   library(data.table)
-  library(dplyr)
+  library(tidyverse)
 }))
 
 inFile = args[1]
-outEven = args[2]
-outAll = args[3]
+outFile = args[2]
+type = args[3]
 
 # Read in SNPs
-dfSNPs <- fread(inFile)
+dfSNPs <- fread(inFile, header=FALSE)
+print(head(dfSNPs))
 
-# Save all SNPs
-fwrite(dfSNPs, outAll, row.names = F, col.names = F, quote = F, sep = "\t")
-print(nrow(dfSNPs))
-
-# Pick even snps
-dfSNPs <- dfSNPs %>% separate(V1, into=c("chr", "pos"), sep = ":", keep = TRUE)
-dfSNPs <- dfSNPs %>% filter(chr %in% c("2", "4", "6", "8", "10", "12", "14", "16", "18", "20", "22")) %>% select(V1)
-print(nrow(dfSNPs))
-fwrite(dfSNPs, outAll, row.names = F, col.names = F, quote = F, sep = "\t")
-
+if (type == "chr-all") {
+   # Save all SNPs
+   fwrite(dfSNPs, outFile, row.names = F, col.names = F, quote = F, sep = "\t")
+   print(nrow(dfSNPs))
+} else if (type == "chr-even") {
+  # Pick even snps
+  dfSNPs <- dfSNPs %>% separate(V1, into=c("chr", "pos"), sep = ":",  remove = FALSE)
+  dfSNPs <- dfSNPs %>% filter(chr %in% c("2", "4", "6", "8", "10", "12", "14", "16", "18", "20", "22")) %>% select(V1)
+  print(nrow(dfSNPs))
+  fwrite(dfSNPs, outFile, row.names = F, col.names = F, quote = F, sep = "\t")
+}
 
 
 
