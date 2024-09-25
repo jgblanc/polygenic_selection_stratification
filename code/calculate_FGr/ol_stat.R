87695952761cee75d4d0c47af4913731e04b82ef
Jennifer Blanc <jenniferblanc@HGD-ML-33.local>
2024-09-24 19:55:57 -0500

    pls merget

--- code/calculate_FGr/ol_stat.R
+++ code/calculate_FGr/ol_stat.R
@@ -9,13 +9,34 @@ suppressWarnings(suppressMessages({
   library(dplyr)
 }))
 
-infile = args[1]
-outfile = args[2]
+infile_betas = args[1]
+infile_var = args[2]
+outfile = args[3]
 
 
-df <- fread(infile)
-df <- df[, c(1,2,4)]
-df$FGr <- scale(df$FGr)
+beta_prefix =
+# Get list of all chromosomes
+chrs <- seq(1,22)
+
+# Add Tm for each chromosome to each other
+data <- fread(paste0(Tm_prefix, "_1.txt"))
+data <- data[,4:ncol(data)]
+
+for (i in 2:22) {
+
+  print(paste0("chr num ",i))
+  # Read in new chromosome
+  filename <- paste0(Tm_prefix,"_", i, ".txt")
+  tmp <- fread(filename)
+  tmp <- tmp[,4:ncol(tmp)]
+  data <- cbind(data, tmp)
+
+}
+data <- as.data.frame(data)
+print(dim(data))
+
+dfBeta <- fread(infile_betas)
+dfVar <- fread(infile)
 print(head(df))
 
 fwrite(df, outfile, row.names = F, col.names = T, quote = F, sep = "\t")
