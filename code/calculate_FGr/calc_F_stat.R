5d441841fd083ab3d9400b3b0ad3dbd9b382c2e0
Jennifer Blanc <jenniferblanc@HGD-ML-33.local>
2024-09-25 14:36:27 -0500

    t

--- code/calculate_FGr/calc_F_stat.R
+++ code/calculate_FGr/calc_F_stat.R
@@ -13,7 +13,6 @@ suppressWarnings(suppressMessages({
 betas_prefix = args[1]
 var_prefix = args[2]
 outfile = args[3]
-ldFile = args[4]
 
 
 # Get list of all chromosomes
@@ -58,32 +57,9 @@ df$BETA <- df$BETA * (sqrt(df$Var))
 H <- mean(df$BETA^2)
 print(H)
 
-print(head(df))
-# Read in LD block file
-ld <- fread(ldFile)
-
-# Assign SNPs to blocks
-assign_SNP_to_block <- function(CHR, BP, block = ld) {
-
-  # Filter blocks based on snp
-  block_chr <- block %>% filter(chr == CHR)
-  first_start <- as.numeric(block_chr[1, "start"])
-  block_bp <- block_chr %>% filter( (start < BP & stop >= BP) | BP == first_start)
-
-  # Assign
-  block_num <- as.numeric(block_bp[,"block_number"])
-  return(block_num)
-}
-
-# Add block info - takes a while
-df <- df %>%
-  mutate(block = apply(., MARGIN = 1, FUN = function(params)assign_SNP_to_block(as.numeric(params[1]), as.numeric(params[2])))) %>%
-  drop_na()
-print(paste0("Now df blocks has", nrow(df), " rows"))
-print(head(df))
 
 # Calculate block jackknife for H
-nblocks <- unique(df$block)
+nblocks <- length(unique(df$block))
 L <- nrow(df)
 allHs <- rep(NA, nblocks)
 for (i in 1:nblocks) {
